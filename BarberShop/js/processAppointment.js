76e00d5301168578fb23edd98f5cd1582b2be0fe
JohannOlivares <60246437+JohannOlivares@users.noreply.github.com>
2021-06-09 01:03:37 -0700

    Add files via upload

---
+++ BarberShop/js/processAppointment.js
@@ -0,0 +1,7 @@
+/*
+ * Author: Johann Olivares
+ * File: processAppointment.js
+ * Date: 06/01/2021
+ * Description: This script houses the logic that
+ *              will process and store appoitments.
+ *  */
